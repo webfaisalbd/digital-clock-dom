3d129df37bf9425ea59f907f2df4e698bf711013
faisalcsebd <faisal.cse.bd@gmail.com>
2021-09-06 13:27:51 +0600

    complete

--- js/app.js
+++ js/app.js
@@ -4,13 +4,40 @@ const digitalClock = () => {
     let minutes = moment.getMinutes();
     let seconds = moment.getSeconds();
     let timeFormat = 'AM';
+
+    // time format change 
+    if (hours === 0) {
+        hours = 12;
+    }
+    if (hours > 12) {
+        hours = hours - 12;
+        timeFormat = 'PM';
+    }
+
+    /*
+        if (hours < 10) {
+            hours = '0' + hours;
+        }
+        if (minutes < 10) {
+            minutes = '0' + minutes;
+        }
+        if (seconds < 10) {
+            seconds = '0' + seconds;
+        }
+    */
+    // using ternary operator for avoiding single digit output
+    hours = hours < 10 ? '0' + hours : hours;
+    minutes = minutes < 10 ? '0' + minutes : minutes;
+    seconds = seconds < 10 ? '0' + seconds : seconds;
+
+    // total time 
     let finalTime = `${hours}:${minutes}:${seconds}`;
 
     // show html file 
     document.getElementById('time').innerText = finalTime;
     document.querySelector('small').innerText = timeFormat;
 
-    console.log(finalTime);
+    // console.log(finalTime);
+    setInterval(digitalClock, 1000);
 }
-
-setInterval(digitalClock, 1000);
+digitalClock();
